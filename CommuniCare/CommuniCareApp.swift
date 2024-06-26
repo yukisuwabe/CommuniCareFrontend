ef7e9cd714163562a6d7714910fb8f57d2d638a0
Adelynn <hw769@cornell.edu>
2024-06-26 13:19:30 +0800

    Initial Commit

---
+++ CommuniCare/CommuniCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommuniCareApp.swift
+//  CommuniCare
+//
+//  Created by Hsia Lu wu on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct CommuniCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
